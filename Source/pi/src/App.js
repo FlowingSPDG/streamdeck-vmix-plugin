c41264f472453c20bc6746332090e3d021f62a0d
Shugo Kawamura <shugo_k@flowing.tokyo>
2021-04-24 02:21:59 +0900

    Fix selected state

--- Source/pi/src/App.js
+++ Source/pi/src/App.js
@@ -187,24 +187,19 @@ class FunctionInput extends React.Component {
 class InputList extends React.Component {
   constructor(props){
     super(props)
-    this.state = {
-      selected_key: props.selected_key // Selected input key
-    }
-
     this.setSelected = this.setSelected.bind(this);
   }
   setSelected(input_key){
-    this.setState({ selected_key:input_key })
     this.props.setSelected(input_key)
   }
   render() {
     return <div type="list" className="sdpi-item list">
       <div className="sdpi-item-label">Inputs List</div>
       <div className="sdpi-item-value single-select" type="">
-        <Input onClick={()=>{this.setSelected("")}} selected={this.state.selected_key === "" } input_key={""} id={`input_list_NONE`} content={`NONE`} ></Input>
-        <Input onClick={()=>{this.setSelected("0")}} selected={this.state.selected_key === "0" } input_key={"0"} id={`input_list_PRV`} content={`Preview`} ></Input>
-        <Input onClick={()=>{this.setSelected("-1")}} selected={this.state.selected_key === "-1" } input_key={"-1"} id={`input_list_PGM`} content={`Program`} ></Input>
-        { this.props.inputs.map((input) => <Input onClick={()=>{this.setSelected(input.Key)}} selected={this.state.selected_key === input.Key } input_key={input.key} id={`input_list_${input.Number}`} content={`${input.Number} : ${input.Name}`} ></Input>) }
+        <Input onClick={()=>{this.setSelected("")}} selected={this.props.selected_key === "" } input_key={"NONE"} key={"NONE"} id={`input_list_NONE`} content={`NONE`} ></Input>
+        <Input onClick={()=>{this.setSelected("0")}} selected={this.props.selected_key === "0" } input_key={"0"} key={"PRV"} id={`input_list_PRV`} content={`Preview`} ></Input>
+        <Input onClick={()=>{this.setSelected("-1")}} selected={this.props.selected_key === "-1" } input_key={"-1"} key={"PGM"} id={`input_list_PGM`} content={`Program`} ></Input>
+        { this.props.inputs.map((input) => <Input onClick={()=>{this.setSelected(input.Key)}} selected={this.props.selected_key === input.Key } input_key={input.key} key={input.key} id={`input_list_${input.Number}`} content={`${input.Number} : ${input.Name}`} ></Input>) }
       </div>
     </div>
   }
@@ -212,7 +207,7 @@ class InputList extends React.Component {
 
 class Input extends React.Component {
   render() {
-    return <li id={this.props.id} key={this.props.input_key} className={this.props.selected ? "selected" : ""} onClick={this.props.onClick} >{this.props.content}</li>
+    return <li id={this.props.id} input_key={this.props.input_key} className={this.props.selected ? "selected" : ""} onClick={this.props.onClick} >{this.props.content}</li>
   }
 }
 
