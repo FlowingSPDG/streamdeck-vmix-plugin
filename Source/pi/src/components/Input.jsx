9566ec9d85f9c5f11aaefa81e424ad88ade8179b
Shugo Kawamura <kawamura@giken.tv>
2021-04-25 18:50:56 +0900

    Fix selected state

--- Source/pi/src/components/Input.jsx
+++ Source/pi/src/components/Input.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 class Input extends React.Component {
     render() {
-      return <li id={this.props.id} input_key={this.props.input_key} className={this.props.selected ? "selected" : ""} onClick={this.props.onClick} style={{ textAlign:"left" }} >{this.props.content}</li>
+      return <li id={this.props.id} input_key={this.props.input_key} className={this.props.selected === true ? "selected" : ""} onClick={this.props.onClick} style={{ textAlign:"left" }} >{this.props.content}</li>
     }
 }
 
