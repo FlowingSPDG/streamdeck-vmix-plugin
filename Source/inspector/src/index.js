eda7ad1790f56c0ccd393ecf681b439bb76a2738
Shugo Kawamura <ruk1ium@gmail.com>
2024-05-20 17:35:33 +0900

    remove old PI, added react-based pi

--- Source/inspector/src/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
-import "./index.css";
-import ConfigView from "./ConfigView";
-
-ReactDOM.render(<ConfigView />, document.getElementById("root"));
