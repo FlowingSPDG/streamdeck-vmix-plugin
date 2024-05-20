3ae0c3c9defcd4f6b18706a3b1e018af5ec022d4
FlowingSPDG <ruk1ium@gmail.com>
2024-05-20 16:33:53 +0900

    backup

---
+++ Source/inspector/src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./index.css";
+import ConfigView from "./ConfigView";
+
+ReactDOM.render(<ConfigView />, document.getElementById("root"));
