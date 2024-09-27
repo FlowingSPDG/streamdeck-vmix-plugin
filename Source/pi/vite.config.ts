045f5316465bf087a9562cd99c132db835ca5fb9
naporin0624 <naporin0624@gmail.com>
2024-09-27 09:03:33 +0000

    chore: use singleFile plugin

--- Source/pi/vite.config.ts
+++ Source/pi/vite.config.ts
@@ -1,10 +1,14 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { viteSingleFile } from 'vite-plugin-singlefile'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), viteSingleFile()],
+  build: {
+    target: 'es2018',
+  },
   test: {
     globals: true,
     environment: 'jsdom',
