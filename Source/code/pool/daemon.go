741631b550c2e68fccb514f8b2dbce236385e0fb
Shugo Kawamura <shugo_k@flowing.tokyo>
2024-10-29 14:30:41 +0900

    vMix connection pool now handles TCP connection successfully including Tally

--- Source/code/pool/daemon.go
+++ Source/code/pool/daemon.go
@@ -2,7 +2,6 @@ package pool
 
 import (
 	"context"
-	"log"
 	"time"
 
 	vmixtcp "github.com/FlowingSPDG/vmix-go/tcp"
@@ -19,7 +18,7 @@ func (v *vmixPool) startRetry(ctx context.Context, host string) {
 				return
 			case <-time.After(5 * time.Second):
 				if err := v.retry(ctx, host); err != nil {
-					log.Printf("failed to retry: %v", err)
+					v.logger.LogMessage(ctx, "failed to retry: %v", err)
 				}
 			}
 		}
@@ -33,24 +32,41 @@ func (v *vmixPool) retry(ctx context.Context, host string) error {
 	// vMixのインスタンスが削除されている場合、再接続処理を行わない
 	vi, ok := v.vs.Load(host)
 	if !ok {
+		v.logger.LogMessage(ctx, "destination %s is probably deleted. Abort!", host)
+
 		return nil
 	}
 
 	if vi.vmix.IsConnected() {
+		v.logger.LogMessage(ctx, "destination %s is already connected. Abort!", host)
+
 		return nil
 	}
+
+	v.logger.LogMessage(ctx, "Trying to connect destination %s ...", host)
 
 	// 1.接続処理を行う
 	if err := vi.vmix.Connect(vi.ctx, time.Second); err != nil {
 		return xerrors.Errorf("failed to connect to vMix: %w", err)
 	}
+	v.logger.LogMessage(ctx, "connected to vMix destination %s. Register callbacks...", host)
+
 	// 2: コールバックを登録する
+	vi.vmix.OnVersion(func(vr *vmixtcp.VersionResponse) {
+		// バージョン情報を受け取ったときの処理
+		v.logger.LogMessage(ctx, "VersionResponse: %v", vr)
+		if err := vi.vmix.Subscribe(vmixtcp.EventTally, ""); err != nil {
+			panic(err)
+		}
+	})
 	vi.vmix.OnTally(func(tr *vmixtcp.TallyResponse) {
 		// タリーリクエストを受け取ったときの処理
 		// (controllerに処理を委譲する)
 		// → つまりControllerを内部に持つ必要がある？
-		log.Println("TallyResponse: ", tr)
+		v.logger.LogMessage(ctx, "TallyResponse: %v", tr)
 	})
+
+	v.logger.LogMessage(ctx, "Registered callbacks %s", host)
 
 	// 3: 接続に成功したら、vMixの状態を監視する
 	if err := vi.vmix.Run(vi.ctx); err != nil {
